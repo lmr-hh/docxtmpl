f87b6d16abb8a83e50b0d9cb10b4d3f58290b583
Kim Wittenburg <kim@codello.dev>
2023-10-27 13:39:25 +0200

    Remove token whitespace

--- docxtmpl/app.py
+++ docxtmpl/app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 
 if os.getenv("AUTH_TOKEN_FILE"):
     with open(os.getenv("AUTH_TOKEN_FILE"), "r") as f:
-        auth_token = f.readline()
+        auth_token = f.readline().strip()
 if os.getenv("AUTH_TOKEN"):
     auth_token = os.getenv("AUTH_TOKEN")
-
-print(f"Token: {auth_token}")
